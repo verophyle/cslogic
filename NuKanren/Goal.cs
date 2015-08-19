21c61a65b039267e898ead95a1fc8af83436f94e
Gordon Tisher <gordon@balafon.net>
2015-08-19 16:20:29 -0700

    initial project

---
+++ NuKanren/Goal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuKanren
+{
+    public abstract class Goal
+    {
+    }
+
+    public class Unif : Goal
+    {
+    }
+}
---
+++ NuKanren/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NuKanren")]
+[assembly: AssemblyDescription("An implementation of MicroKanren for .NET.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Verophyle Informatics")]
+[assembly: AssemblyProduct("NuKanren")]
+[assembly: AssemblyCopyright("Copyright © Verophyle Informatics 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f9ea49b-7201-4ecd-bb9e-2df8e35092dd")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0")]
